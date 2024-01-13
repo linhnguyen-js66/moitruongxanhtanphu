ca5115b8c68f773702cefb0f089d2cd306c9fa78
linhnh8 <linhnh8@pvcombank.com.vn>
2024-01-13 17:21:12 +0700

    maintain notify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { IconSvgLocal } from '@/components';
 import HomeLayout from '@/layouts/home-layout';
 import { Meta } from '@/layouts/Meta';
 import { AppConfig } from '@/utils/AppConfig';
@@ -6,15 +7,27 @@ import type { NextPageWithLayout } from './_app';
 
 const Home: NextPageWithLayout = () => {
   return (
-    <Meta title={AppConfig.site_name} description={AppConfig.description}>
+    <>
+      <Meta title={AppConfig.site_name} description={AppConfig.description} />
       <div className="wrap_content_page">
         <div className="content_page">
           <div className="relative">
-            <div className="relative z-10">{}</div>
+            <div className="relative z-10">
+              <div className="flex h-screen w-screen flex-col items-center justify-center">
+                <div className="over-hidden flex h-[700px] w-[700px] flex-col items-center justify-center rounded-[50%] bg-secondary-50 p-32 text-center">
+                  <IconSvgLocal height={300} name="IC_MAINTAIN" />
+                  <div className="h2 mt-24 text-secondary-600 ">Trang web đang được bảo trì</div>
+                  <div className="body3 mt-4 text-secondary-600 ">
+                    Chúng tôi đang cố gắng khắc phục sự cố sớm nhất
+                  </div>
+                  <div />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </Meta>
+    </>
   );
 };
 Home.Layout = HomeLayout;
